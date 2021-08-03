eedba5b835a4ff42a86584a80b72078b7e8f1b97
luca <luca.lorenzetti0@gmail.com>
2021-08-03 11:54:26 +0200

    add operations

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,18 +1,107 @@
 // OPERATIONS FUNCTIONS
 
+console.log("÷".charCodeAt(0))
+function checkExpression(operator){
 
-function addText(value){
+    
+    let op = "";
+    let lastText = $('#old_display_text').text();
+    let currentText = $('#display_text').text();
 
-    if( value != "."){
-        if($('#display_text').text() == 0)
-            $('#display_text').text(value );
+    // let lastElement = lastText[lastText.length-1];
 
+    const operators = ['+', '-' , '/', '*', '106', '111', '107', '109'];
+
+    if(operators.includes(operator.toString())){
+
+        if(!isNaN(operator)){
+            op = convertCharacter(operator);
+        }
+        if(lastText != 0 && !lastText.includes("=")){
+         
+            let expression = lastText.toString() + currentText.toString();
+            $('#old_display_text').text(getResult(expression) + op);
+
+        }
         else{
-            $('#display_text').text( $('#display_text').text() + value );
+            $('#old_display_text').text(currentText+op);
+        }
+        $('#display_text').text("0");
+   
+    }
+    else{
+      
+        if(!lastText.includes("=")){
+            let expression = lastText.toString() + currentText.toString();
+
+            $('#old_display_text').text(expression + " =");
+            $('#display_text').text( getResult(expression) );
+        }
+  
+
+    }
+
+
+
+function getResult(expression){
+    
+    return math.evaluate(expression);
+    }
+}
+function isSpecialCharacter(keyCode){
+    const characters = ['190','110','188','189','109','106','107', '187', '197','111','106','43','47', '42'];
+
+
+    if( characters.includes(keyCode.toString())){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+function convertCharacter(keyCode){
+    let characters = {
+        '190' : '.',
+        '110' : '.',
+        '188' : ',',
+        '189' : '-',
+        '109' : '-',
+        '107' : '+',
+        '187' : '+',
+        '43': '+',
+        '111' : '/',
+        '47' : '/',
+        '106' : '*',
+        '42' : '*'
+    };
+
+   return characters[keyCode];
+}
+function addText(keyCode){
+
+    if( !isSpecialCharacter(keyCode) ){
+        if( (parseInt(keyCode) >= 48 && parseInt(keyCode) <= 57) ||(parseInt(keyCode) >= 96 && parseInt(keyCode) <= 105) ){
+
+            if((parseInt(keyCode) >= 96 && parseInt(keyCode) <= 105)){
+                keyCode -= 48;
+            }
+
+            if($('#display_text').text() == 0){
+                $('#display_text').text(String.fromCharCode(keyCode) );
+            }
+
+            else{
+                if( String.fromCharCode(keyCode)){
+                    $('#display_text').text( $('#display_text').text() + String.fromCharCode(keyCode) );
+                }
+            }
         }
     }
     else{
-        $('#display_text').text( $('#display_text').text() + value );
+        $('#display_text').text( $('#display_text').text() + convertCharacter(keyCode) );
+
     }
 }
 
@@ -36,15 +125,25 @@ function checkKeyUp(key){
 
 $(document).ready(()=>{
     $('button').click((e)=>{
-    })
+        let val = $(e.target).val();
+
+        console.log(val.charCodeAt(0))
+        addText(val.charCodeAt(0));
+    });
 
 
-    $('html').keyup((e)=>{
+    $('html').keydown((e)=>{
+
+
         if(e.which == 8){
             checkKeyUp("del");
         }
+        else if( e.which == '13' || e.which == '106' || e.which == '111' || e.which ==  '107' || e.which == '109'){
+
+            checkExpression(e.which);
+        }
         else{
-            checkKeyUp(String.fromCharCode(e.which));
+            checkKeyUp(e.which);
         }
     });
 });
